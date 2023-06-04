5d0802a5bae590823750ad9f4308992c33ce479a
Finn Jesse Dan <jessedan160@gmail.com>
2023-06-04 01:44:10 +0100

    add web.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'bloc_obsever.dart';
-import 'presentation/controllers/MenuAppController.dart';
 import 'src/app.dart';
 
 void main() async {
